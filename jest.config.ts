82add706e8ad2b6d8efd04704c69ba8e4804fad4
Sole Cold <cool.gegeg@gmail.com>
2021-12-22 11:08:44 +0200

    Initial commit

---
+++ jest.config.ts
@@ -0,0 +1,11 @@
+import type { Config } from '@jest/types';
+
+const config: Config.InitialOptions = {
+    verbose: true,
+    preset: 'ts-jest',
+    testEnvironment: 'node',
+    setupFilesAfterEnv: ['./jest/jest-setup.ts'],
+    roots: ['<rootDir>/tests']
+}
+
+export default config;
---
+++ tests/initial-test.test.ts
@@ -0,0 +1,7 @@
+describe('Initial test', () => {
+
+
+  it('should execute initial test', async () => {
+    expect(2 + 2).toEqual(4);
+  });
+});
