db2dd7a25afae5ae5cc7fd31302fd6b615613a2f
Sole Cold <cool.gegeg@gmail.com>
2022-02-11 21:13:43 +0200

    jest new tsconfig setup #10

--- src/common/models/default-body-error.model.ts
+++ src/common/models/default-body-error.model.ts
@@ -4,6 +4,6 @@ import { StatusCodes } from 'http-status-codes';
 
 export class DefaultBodyErrorModel extends BaseError {
     constructor(public readonly fields: ErrorField[]) {
-        super(StatusCodes.BAD_REQUEST, 'Received invalid body fields');
+        super(StatusCodes.BAD_REQUEST, 'Received invalid values');
     }
 }
