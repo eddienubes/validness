2a91e3649647568a355f314e151e54290ed7f0b1
Sole Cold <cool.gegeg@gmail.com>
2022-02-13 10:34:38 +0200

    fixed #12 class-transformer issue and changed class constructor approach a bit

--- src/common/types/types.ts
+++ src/common/types/types.ts
@@ -1,5 +1,1 @@
-import { ErrorField } from '../models/error-field.model';
-
-export type AnyObject = Record<string, any>;
-export type ClassConstructor = { new (...args: unknown[]): AnyObject };
-export type ErrorConstructor = { new (errors: ErrorField[], ...args: unknown[]): AnyObject };
+export type AnyObject = Record<string | number | symbol, any>;
--- src/validation-body-pipe/validation-body-pipe.ts
+++ src/validation-body-pipe/validation-body-pipe.ts
@@ -1,16 +1,16 @@
-import { ClassConstructor } from '../common/types/types';
 import { NextFunction, RequestHandler, Response } from 'express';
 import { plainToClass } from '@nestjs/class-transformer';
 import { validateOrReject, ValidatorOptions } from '@nestjs/class-validator';
 import { findViolatedFields } from '../utils/find-violated-fields';
 import { ValidationError } from '@nestjs/class-validator';
-import { ErrorConstructor } from '../common/types/types';
 import { DefaultBodyErrorModel } from '../common/models/default-body-error.model';
 import { DEFAULT_BODY_VALIDATOR_CONFIG } from '../common/constants/validator';
 import { ConfiguredRequest } from '../common/interfaces/ConfiguratedRequest';
+import { ClassConstructor } from '../common/models/class-constructor.model';
+import { UserCustomError } from '../common/models/user-custom-error.model';
 
 export const validationBodyPipe =
-    (DtoConstructor: ClassConstructor, UserCustomError?: ErrorConstructor, validatorConfig?: ValidatorOptions): RequestHandler =>
+    (DtoConstructor: ClassConstructor, UserError?: typeof UserCustomError, validatorConfig?: ValidatorOptions): RequestHandler =>
     async (req: ConfiguredRequest, res: Response, next: NextFunction): Promise<void> => {
         const { body } = req;
 
@@ -20,10 +20,12 @@ export const validationBodyPipe =
 
         try {
             await validateOrReject(instance, config);
+
+            req.body = instance;
         } catch (e) {
             const errors = findViolatedFields(e as ValidationError[]);
 
-            return next(UserCustomError ? new UserCustomError(errors) : new DefaultBodyErrorModel(errors));
+            return next(UserError ? new UserError(errors) : new DefaultBodyErrorModel(errors));
         }
 
         return next();
